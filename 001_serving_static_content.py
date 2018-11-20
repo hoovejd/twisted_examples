2968ebca726f6692ad28d607dcaeacefd73ecde1
Jarrod Hoover <hoov85@gmail.com>
2018-11-20 14:25:23 -0500

    added first two twisted tutorials

---
+++ 001_serving_static_content.py
@@ -0,0 +1,12 @@
+from twisted.web.server import Site
+from twisted.web.static import File
+from twisted.internet import reactor, endpoints
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+resource = File(os.path.join(dir_path, "tmp"))
+factory = Site(resource)
+endpoint = endpoints.TCP4ServerEndpoint(reactor, 8888)
+endpoint.listen(factory)
+reactor.run()
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # twisted_examples
-
 These examples are from the Twisted Web in 60 Seconds samples: https://twistedmatrix.com/documents/current/web/howto/web-in-60/index.html
+
+### 001
+To test this navigate to **http://localhost:8888**
+
+### 002
+To test this navigate to **http://localhost:8080**
